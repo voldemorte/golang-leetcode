6c3f0c22d9e878cc10872ae4333505e65beb6ce7
Nikhil Chaudhari <nikhil.chaudhari@gojek.com>
2022-09-10 11:38:20 +0530

    solution to L160

---
+++ easy/l160.go
@@ -0,0 +1,27 @@
+package easy
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	var ptrA, ptrB = headA, headB
+
+	for ptrA != ptrB {
+		if ptrA == nil {
+			ptrA = headB
+		} else {
+			ptrA = ptrA.Next
+		}
+
+		if ptrB == nil {
+			ptrB = headA
+		} else {
+			ptrB = ptrB.Next
+		}
+	}
+	return ptrA
+}
